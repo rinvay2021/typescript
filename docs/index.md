513b4f39609201db4fe1da6941204e10543e2d9e
rinvay <rinvay1999@qq.com>
2023-12-28 23:58:28 +0800

    feat:完善文档

---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+pageType: home
+
+hero:
+    name: TypeScript
+    tagline: TypeScript介绍
+    actions:
+        - theme: brand
+          text: 快速开始
+          link: /document/1.基础类型
+---
--- rspress.config.ts
+++ rspress.config.ts
@@ -1,5 +1,20 @@
-import { defineConfig } from "rspress/config";
+import { defineConfig } from 'rspress/config'
 
 export default defineConfig({
-  root: "docs",
-});
+    root: 'docs',
+    title: 'ts',
+    icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968421.png',
+    logo: {
+        dark: 'https://www.endpointdev.com/blog/2021/02/intro-to-typescript/ts-lettermark-white.svg',
+        light: 'https://camo.githubusercontent.com/e0006a1bf723541f065116ce508b54dd9cb90e7229fb19a7ac7aad1a0bfb999e/68747470733a2f2f63646e2e737667706f726e2e636f6d2f6c6f676f732f747970657363726970742e737667',
+    },
+    themeConfig: {
+        socialLinks: [
+            {
+                icon: 'github',
+                mode: 'link',
+                content: 'https://github.com/rinvay2021/typescript',
+            },
+        ],
+    },
+})
